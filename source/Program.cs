d45a8128a51ed04d916a6f8972ba2792a7c44e9e
Milo M. Cruz <milo.cruz.2011@gmail.com>
2025-06-08 22:03:01 -0400

    Increased Variety and Smoothness

--- source/Program.cs
+++ source/Program.cs
@@ -1,10 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 class Program
 {
-    const int minTrail = 4;
-    const int maxTrail = 12;
+    class Stream
+    {
+        public int X;
+        public float Y;
+        public float Speed;
+        public int TrailLength;
+        public int Delay;
+        public int Height;
+        public Queue<int> Trail = new();
+
+        private static Random rand = new Random();
+
+        public Stream(int x, int screenHeight)
+        {
+            X = x;
+            Height = screenHeight;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Y = rand.Next(-Height, 0);
+            Speed = (float)(0.5 + rand.NextDouble() * 1.5);
+            TrailLength = rand.Next(6, 20);
+            Delay = rand.Next(0, 50);
+            Trail.Clear();
+        }
+
+        public void Update()
+        {
+            if (Delay > 0)
+            {
+                Delay--;
+                return;
+            }
+
+            Y += Speed;
+
+            if (Y >= Height + TrailLength)
+                Reset();
+        }
+
+        public void Draw()
+        {
+            if (Delay > 0) return;
+
+            for (int i = 0; i < TrailLength; i++)
+            {
+                int yPos = (int)(Y - i);
+                if (yPos < 0 || yPos >= Height) continue;
+
+                Console.SetCursorPosition(X, yPos);
+
+                if (i == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (i < TrailLength / 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                }
+
+                char c = (char)(rand.Next(0x30A0, 0x30FF));
+                Console.Write(c);
+            }
+
+            // Erase one below trail
+            int clearY = (int)(Y - TrailLength);
+            if (clearY >= 0 && clearY < Height)
+            {
+                Console.SetCursorPosition(X, clearY);
+                Console.Write(' ');
+            }
+        }
+    }
 
     static void Main()
     {
@@ -14,54 +92,24 @@ class Program
         int width = Console.WindowWidth;
         int height = Console.WindowHeight;
 
-        Random rand = new Random();
-
-        // Each column will have a Y position and a trail length
-        int[] yPositions = new int[width];
-        int[] trailLengths = new int[width];
-
-        for (int i = 0; i < width; i++)
+        List<Stream> streams = new();
+        for (int x = 0; x < width; x++)
         {
-            yPositions[i] = rand.Next(height);
-            trailLengths[i] = rand.Next(minTrail, maxTrail);
+            if (new Random().NextDouble() < 0.6) // 60% of columns active
+            {
+                streams.Add(new Stream(x, height));
+            }
         }
 
         while (true)
         {
-            for (int x = 0; x < width; x++)
+            foreach (var stream in streams)
             {
-                int y = yPositions[x];
-
-                // Draw head (bright white)
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.SetCursorPosition(x, y);
-                Console.Write((char)rand.Next(0x30A0, 0x30FF)); // Katakana characters
-
-                // Draw trail (green)
-                for (int j = 1; j < trailLengths[x]; j++)
-                {
-                    int trailY = (y - j + height) % height;
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.SetCursorPosition(x, trailY);
-                    Console.Write((char)rand.Next(0x30A0, 0x30FF));
-                }
-
-                // Clean up old trail
-                int eraseY = (y - trailLengths[x] + height) % height;
-                Console.SetCursorPosition(x, eraseY);
-                Console.Write(' ');
-
-                // Update position
-                yPositions[x] = (y + 1) % height;
-
-                // Occasionally reset trail length for variety
-                if (rand.NextDouble() < 0.01)
-                {
-                    trailLengths[x] = rand.Next(minTrail, maxTrail);
-                }
+                stream.Update();
+                stream.Draw();
             }
 
-            Thread.Sleep(75); // ~13 fps, feels smooth-ish
+            Thread.Sleep(33); // ~30 FPS
         }
     }
 }
